5563d1bf2b6230c2b9dcd60d697602556410487b
rosoporto <rosoporto@yandex.ru>
2024-01-18 00:04:37 +0300

    add module dotenv

--- ptr/main.py
+++ ptr/main.py
@@ -1,8 +1,14 @@
+import os
+from dotenv import load_dotenv
 from colorama import Fore
 
 def main():
+    
     print(Fore.YELLOW + 'I\'am main')
+
+    print(os.getenv('DB__USER'))
 
 
 if __name__ == '__main__':
+    load_dotenv()
     main()
